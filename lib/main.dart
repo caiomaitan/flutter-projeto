83cbaf30b4754a64c230a1707b95676aaf84cd8e
jaodoka02 <joaovbernardes02@gmail.com>
2025-04-08 13:30:49 -0300

    colocando o device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // Import necessário
+import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart'; // Importe o device_preview
 import 'pages/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options:
-        DefaultFirebaseOptions
-            .currentPlatform, // Inicializa com as opções corretas do Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    DevicePreview(
+      enabled: true, // Ativa o DevicePreview
+      builder: (context) => const MyApp(),
+    ),
   );
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +22,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true, // Necessário para o device_preview
+      locale: DevicePreview.locale(context), // Usa o locale do device_preview
+      builder: DevicePreview.appBuilder, // Constrói com base no device_preview
       title: 'Meu App',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const LoginPage(),
